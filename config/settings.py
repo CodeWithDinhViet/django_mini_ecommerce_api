3663fd1a27a7b988ffb30a5284be0ba58e72ac6e
Ledinhviet1210 <Ledinhviet12102004@gmail.com>
2026-06-02 09:47:00 +0700

    Add pagination

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,8 @@ REST_FRAMEWORK = {
     ),
     
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 5,
 }
 
 SPECTACULAR_SETTINGS = {
